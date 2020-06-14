914c2afabe272cd1dc41e0f02e1788aea6991cd4
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-06-14 18:05:48 +0530

    Second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './components/Layouts/layout'
+
+function App() {
+  return (
+    <div>
+      HELLO
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
